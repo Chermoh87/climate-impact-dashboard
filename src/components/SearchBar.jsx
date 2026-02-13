fc7a57fb9dc615ed6f0e1c108ea799d96a0b946c
chermoh <itz.chermoh@gmail.com>
2026-02-13 22:26:47 +0100

    feat: Added Header and SearchBar component and integrated into App

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter city name" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
